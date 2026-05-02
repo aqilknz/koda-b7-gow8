390046b3f8c17d629af4912bee3048aedc4a9e52
aqilknz <khairunnz123@gmail.com>
2026-05-02 12:53:53 +0700

    feat: add task 2 and task 4 features in Main

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"go-weekly/internals/task1"
+	"go-weekly/internals/task2"
 	"go-weekly/internals/task3"
+	"go-weekly/internals/task4"
+	"sync"
 )
 
 func main() {
@@ -16,6 +19,33 @@ func main() {
 	task1.ProcessNumber(input)
 	task1.ProcessNumber(inputNil)
 	task1.ProcessNumber(inputKosong)
+	fmt.Println("")
+
+	// task 2
+	fmt.Println("Task-2")
+	var wg sync.WaitGroup
+	hasilChannel := make(chan string)
+	listUrls := []string{
+		"https://jsonplaceholder.typicode.com/photos",
+		"https://jsonplaceholder.typicode.com/posts",
+		"https://jsonplaceholder.typicode.com/comments",
+		"https://jsonplaceholder.typicode.com/albums",
+		"https://jsonplaceholder.typicode.com/todos",
+		"https://jsonplaceholder.typicode.com/users",
+	}
+	go func() {
+		for hasil := range hasilChannel {
+			fmt.Println(hasil)
+		}
+		fmt.Println("Semua URL selesai di-fetch!")
+	}()
+	for _, url := range listUrls {
+		wg.Add(1)
+		go task2.WebFetcher(url, hasilChannel, &wg)
+	}
+
+	wg.Wait()
+	close(hasilChannel)
 	fmt.Println("")
 
 	// task 3
@@ -32,5 +62,13 @@ func main() {
 	fmt.Println(um.GetUser(3))
 	// undefined user
 	fmt.Println(um.GetUser(4))
+	fmt.Println("")
+
+	// task 4
+	fmt.Println("Task-4")
+	persegi := task4.Rectangle{Width: 12, Height: 8}
+	lingkaran := task4.Circle{JariJari: 14}
+	fmt.Println(task4.Calculator(persegi))
+	fmt.Println(task4.Calculator(lingkaran))
 
 }
